a45304534ecee62fb9c063de73bfc935f7ad0560
someoneelse0 <someoneelse_000@protonmail.com>
2022-11-16 18:52:12 -0300

    done: program version 1.0

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 let i=document.getElementsByTagName("input")[0];
 let p=document.getElementsByTagName("p");
+let btn=document.getElementsByTagName("button")[0];
+let d=document.getElementById("map");
 let a="";
+let aa=[];
+let la=0;
+let ln=0;
 function f(){
 	axios.get("https://ipinfo.io/"+i.value)
 		.then(function(res0){
-			axios.get("https://json.geoiplookup.io/"+i.value)
-				.then(function(res1){
-					p[0].innerHTML="Data:";
-					p[1].innerHTML="IP: "+res0.data.ip+"<br />Hostname: "+res0.data.hostname+"<br />City: "+res0.data.city+"<br />Region: "+res0.data.region+"<br />Country: "+res0.data.country+"<br />Location: "+res0.data.loc+"<br />Organization: "+res0.data.org+"<br />Postal: "+res0.data.postal+"<br />Timezone: "+res0.data.timezone+"<br />Currency code: "+res1.data.currency_code+"<br />Currency name: "+res1.data.currency_name+"<br />";
-				});
-			p[2].innerHTML="If you want search again, first introduce the new ip and reload this page<br />";
-			//p[3].innerHTML="If you want donate in BTC: A  | Thanks a lot<br />If you want donate in ETH: A  | Thanks a lot<br/>If you want donate in USDT: A  | Thanks a lot<br /><br />";
+			p[0].innerHTML="Data:";
+			p[1].innerHTML="IP: "+res0.data.ip+"<br />Hostname: "+res0.data.hostname+"<br />City: "+res0.data.city+"<br />Region: "+res0.data.region+"<br />Country: "+res0.data.country+"<br />Location: "+res0.data.loc+"<br />Organization: "+res0.data.org+"<br />Postal: "+res0.data.postal+"<br />Timezone: "+res0.data.timezone+"<br />";
+			aa=res0.data.loc.split(",");
+			la=aa[0];
+			ln=aa[1];
+			function fm(){
+			        var m={
+		                	center:new google.maps.LatLng(la,ln),
+		                	zoom:10,
+		                	mapTypeId:google.maps.MapTypeId.HYBRID
+		        	};
+		        	var mp=new google.maps.Map(d,m);
+		        	var mrkr=new google.maps.Marker({
+		        	        position:new google.maps.LatLng(la,ln),
+		        	});
+		        	mrkr.setMap(mp);
+		        	var iw=new google.maps.InfoWindow({
+					content:"IP: "+res0.data.ip+"<br />CITY: "+res0.data.city+"<br />REGION: "+res0.data.region+"<br />COUNTRY: "+res0.data.country+"<br />ORGANIZATION: "+res0.data.org+"<br />TIMEZONE: "+res0.data.timezone,
+		        	});
+		        	google.maps.event.addListener(mrkr,"click",function(){iw.open(map,mrkr);});
+			}
+			google.maps.event.addDomListener(btn,"click",fm);
+			p[2].innerHTML="If you want search again, first introduce the new ip and click the button<br />Note: Click the button again will display place coordinates of address that you has introduced in the following map";
+			p[3].innerHTML="If you want to see place coordinates in the following map, click the button again";
+			p[4].innerHTML="If you want donate in ETH: 0xcD7Bb53A258D441624A913e92D1B49920e2e1B5d | Thanks a lot<br/>If you want donate in USDT: 0xcD7Bb53A258D441624A913e92D1B49920e2e1B5d | Thanks a lot<br /><br />";
 		})
 		.catch(function(error){
 			console.log(error);
 			p[0].innerHTML=error;
 		})
 }
-document.getElementsByTagName("button")[0].addEventListener("click",function(event){
+btn.addEventListener("click",function(event){
 	event.preventDefault();
 	f();
 },false);
